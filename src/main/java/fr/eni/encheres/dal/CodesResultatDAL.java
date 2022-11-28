ac83b0e74411946340c11cf136df634c0a0163b6
jptch <jptch@DESKTOP-J92D8PO>
2022-11-28 11:13:20 +0100

    dal

---
+++ src/main/java/fr/eni/encheres/dal/CodesResultatDAL.java
@@ -0,0 +1,20 @@
+package fr.eni.encheres.dal;
+
+/**
+ * Les codes disponibles sont entre 10000 et 19999
+ */
+public abstract class CodesResultatDAL {
+	public static final int SQL_EXCEPTION= 10000;
+}
+
+
+
+
+
+
+
+
+
+
+
+
