44ae44a6e0e9a914608302196323f3d75feb539a
jptch <jptch@DESKTOP-J92D8PO>
2022-11-29 09:36:52 +0100

    ser

---
+++ src/main/java/fr/eni/encheres/servlet/ServletCreerUtilisateur.java
@@ -0,0 +1,61 @@
+package fr.eni.encheres.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.encheres.BusinessException;
+import fr.eni.encheres.bll.UtilisateurManager;
+import fr.eni.encheres.bo.Utilisateur;
+
+/**
+ * Servlet implementation class ServletCreerUtilisateur
+ */
+@WebServlet("/servletCreerUtilisateur")
+public class ServletCreerUtilisateur extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletCreerUtilisateur() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/CreerUtilisateur.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String pseudo = request.getParameter("pseudo");
+        String nom = request.getParameter("nom");
+        String prenom = request.getParameter("prenom");
+        String email = request.getParameter("email");
+        String telephone = request.getParameter("telephone");
+        String rue = request.getParameter("rue");
+        String codePostal = request.getParameter("codePostal");
+        String ville = request.getParameter("ville");
+        String motDePasse = request.getParameter("motDePasse");
+        Utilisateur u = null;
+        try {
+            u = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse);
+            UtilisateurManager.getInstance().ajouterUtilisateur(u);
+            request.getRequestDispatcher("/WEB-INF/Accueil.jsp").forward(request, response);
+        } catch (BusinessException businessException) {
+            request.setAttribute("listeCodesErreur", businessException.getListeCodesErreur());
+            request.getRequestDispatcher("/WEB-INF/CreerUtilisateur.jsp").forward(request, response);
+        }
+	}
+
+}
