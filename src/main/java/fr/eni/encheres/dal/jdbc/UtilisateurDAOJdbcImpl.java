594c2317e53f9d7cc267c01419ebdb2fc2dcabaa
jptch <jptch@DESKTOP-J92D8PO>
2022-11-29 09:28:08 +0100

    ut

--- src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
+++ src/main/java/fr/eni/encheres/dal/jdbc/UtilisateurDAOJdbcImpl.java
@@ -70,7 +70,36 @@ public void modifierProfil(Utilisateur utilisateur) throws BusinessException {
 	// TODO Auto-generated method stub
 	
 }
-
+private static final String INSERT_UTILISATEUR = "INSERT into UTILISATEURS ( pseudo,  nom,  prenom,  email,  telephone,  rue, codePostal,ville, motDePasse, credit, administrateur) values(?,?,?,?,?,?,?,?,?,?,?);";
+@Override
+public void insert(Utilisateur utilisateur) throws BusinessException {
+    try (Connection cnx = ConnectionProvider.getConnection();
+            PreparedStatement pstmt = cnx.prepareStatement(INSERT_UTILISATEUR,
+                    PreparedStatement.RETURN_GENERATED_KEYS))
+    {
+        pstmt.setString(1, utilisateur.getPseudo());
+        pstmt.setString(2, utilisateur.getNom());
+        pstmt.setString(3, utilisateur.getPrenom());
+        pstmt.setString(4, utilisateur.getEmail());
+        pstmt.setString(5, utilisateur.getTelephone());
+        pstmt.setString(6, utilisateur.getRue());
+        pstmt.setString(7, utilisateur.getCodePostal());
+        pstmt.setString(8, utilisateur.getVille());
+        pstmt.setString(9, utilisateur.getMotDePasse());
+        pstmt.setInt(10, utilisateur.getCredit());
+        pstmt.setBoolean(11, utilisateur.getAdministrateur());
+        pstmt.executeUpdate();
+        ResultSet rs = pstmt.getGeneratedKeys();
+        if (rs.next()) {
+            utilisateur.setIdUtilisateur(rs.getInt(1));
+        }
+    } catch (SQLException e) {
+        BusinessException businessException = new BusinessException();
+        businessException.ajouterErreur(CodesResultatDAL.INSERT_UTILISATEUR_ECHEC);
+        e.printStackTrace();
+        throw businessException;
+    }
+}
 	
 
 }
