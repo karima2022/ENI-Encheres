1dbc7290cb570660898866725461c625517f7f9a
Jo-W7 <Jo-W7@Jo-W7-PC>
2022-11-30 10:35:37 +0100

    Maj

--- src/main/java/fr/eni/encheres/dal/CodesResultatDAL.java
+++ src/main/java/fr/eni/encheres/dal/CodesResultatDAL.java
@@ -6,6 +6,45 @@ package fr.eni.encheres.dal;
 public abstract class CodesResultatDAL {
 	public static final int SQL_EXCEPTION= 10000;
 	public static final int INSERT_UTILISATEUR_ECHEC=10001;
+	
+	
+	
+	// -- Erreurs liées aux articles - 10100 à 10199 --
+	
+		/**
+		 * Echec général quand tentative d'ajouter un objet null
+		 */
+		public static final int INSERT_ARTICLE_NULL = 10100;
+		
+		/**
+		 * Echec général quand erreur non gérée à l'insertion 
+		 */
+		public static final int INSERT_ARTICLE_ECHEC = 10101;
+		
+		/**
+		 * Echec de la lecture d'un article
+		 */
+		public static final int LECTURE_ARTICLE_ECHEC = 10102;
+		
+		/**
+		 * Article inexistant
+		 */
+		public static final int LECTURE_ARTICLE_INEXISTANT = 10103;
+		
+		/**
+		 * Erreur à la suppression d'un article
+		 */
+		public static final int SUPPRESSION_ARTICLE_ERREUR = 10104;
+		
+		/**
+		 * Erreur à la suppression d'un article null
+		 */
+		public static final int SUPPRESSION_ARTICLE_NULL = 10105;
+		
+		/**
+		 * Echec de la lecture des articles
+		 */
+		public static final int LECTURE_ARTICLES_ECHEC = 10106;
 }
 
 
