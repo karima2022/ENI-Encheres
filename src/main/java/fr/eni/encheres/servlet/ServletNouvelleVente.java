5ba14d294563c28b71ee987996d0bceca7d848d9
Jo-W7 <Jo-W7@Jo-W7-PC>
2022-12-01 17:38:56 +0100

    maj

--- src/main/java/fr/eni/encheres/servlet/ServletNouvelleVente.java
+++ src/main/java/fr/eni/encheres/servlet/ServletNouvelleVente.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import fr.eni.encheres.BusinessException;
 import fr.eni.encheres.bll.ArticleManager;
+import fr.eni.encheres.bo.Article;
 
 /**
  * Servlet implementation class ServletNouvelleVente
@@ -39,15 +40,15 @@ public class ServletNouvelleVente extends HttpServlet {
 			if (listeCodesErreur.size() > 0) {
 				request.setAttribute("listeCodesErreur",listeCodesErreur);
 			} else {
+				Article article = new Article();
 				try {
-					articleManager.afficherArticle(idArticle);
+					article = articleManager.afficherArticle(idArticle);
+					request.setAttribute("objArticle", article);
 				} catch (BusinessException e) {
-					e.printStackTrace();
 					request.setAttribute("listeCodesErreur", e.getListeCodesErreur());
 				}
 			}
 			
-//			chargerArticle(request, articleManager);
 		}
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/nouvelle_vente.jsp");
@@ -61,21 +62,40 @@ public class ServletNouvelleVente extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.setCharacterEncoding("UTF-8");
+		String btnSubmit = request.getParameter("btnSubmit");
 		
-		if (request.getParameter("idArticle") != null) {
-			modifierVente(request, response);
-		} else {
-			ajouterNouvelleVente(request, response);
+		
+		if (btnSubmit != null) {
+			if (btnSubmit.equalsIgnoreCase("enregistrer")) {
+				if (request.getParameter("idArticle") != null) {
+					modifierVente(request, response);
+				} else {
+					ajouterNouvelleVente(request, response);
+				}
+			} else if (btnSubmit.equalsIgnoreCase("supprimer")) {
+				ArticleManager articleManager = new ArticleManager();
+				List<Integer> listeCodesErreur = new ArrayList<>();
+				int idArticle = lireParametreIdArticle(request, listeCodesErreur);
+				
+				if (listeCodesErreur.size() > 0) {
+					request.setAttribute("listeCodesErreur",listeCodesErreur);
+				} else {
+					try {
+						articleManager.supprimerArticle(idArticle);
+					} catch (BusinessException e) {
+						request.setAttribute("listeCodesErreur", e.getListeCodesErreur());
+					}
+				}
+			} 
 		}
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Accueil.jsp");
-		rd.forward(request, response);
+		response.sendRedirect("accueil");
 		
 	}
 	
 	
 	
-	private void ajouterNouvelleVente(HttpServletRequest request, HttpServletResponse response) {
+	private void ajouterNouvelleVente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nom;
 		String description;
 		int categorie;
@@ -100,41 +120,78 @@ public class ServletNouvelleVente extends HttpServlet {
 		categorie = Integer.parseInt(request.getParameter("categorie"));
 		prix = Integer.parseInt(request.getParameter("prix"));
 		
-		idVendeur = 1;
+		
+		if (listeCodesErreur.size() > 0) {
+			request.setAttribute("listeCodesErreur",listeCodesErreur);
+			request.getRequestDispatcher("WEB-INF/nouvelle_vente.jsp").forward(request, response);
+		} else {
+			ArticleManager articleManager = new ArticleManager();
+			try {
+				articleManager.ajouterArticle(nom, description, debutEnchere, finEnchere, prix, categorie, idVendeur, rue, codePostal, ville);
+			} catch (BusinessException e) {
+				request.setAttribute("listeCodesErreur",e.getListeCodesErreur());
+				request.getRequestDispatcher("WEB-INF/nouvelle_vente.jsp").forward(request, response);
+			}
+		}
+	}
+	
+	
+	private void modifierVente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idArticle;
+		String nom;
+		String description;
+		int categorie;
+		int prix;
+		LocalDateTime debutEnchere;
+		LocalDateTime finEnchere;
+		String rue;
+		String codePostal;
+		String ville;
+		int idVendeur;
+		List<Integer> listeCodesErreur = new ArrayList<>();
+		
+		idArticle = lireParametreIdArticle(request, listeCodesErreur);
+		nom = lireParametreNomArticle(request, listeCodesErreur);
+		description = lireParametreDescriptionArticle(request, listeCodesErreur);
+		debutEnchere = lireParametreDateDebutEnchere(request, listeCodesErreur);
+		finEnchere = lireParametreDateFinEnchere(request, listeCodesErreur);
+		idVendeur = lireParametreIdVendeur(request, listeCodesErreur);
+		rue = lireParametreRueRetrait(request, listeCodesErreur);
+		codePostal = lireParametreCPRetrait(request, listeCodesErreur);
+		ville = lireParametreVilleRetrait(request, listeCodesErreur);
+		
+		categorie = Integer.parseInt(request.getParameter("categorie"));
+		prix = Integer.parseInt(request.getParameter("prix"));
+		
 		
 		if (listeCodesErreur.size() > 0) {
 			request.setAttribute("listeCodesErreur",listeCodesErreur);
 		} else {
 			ArticleManager articleManager = new ArticleManager();
 			try {
-				articleManager.ajouterArticle(nom, description, debutEnchere, finEnchere, prix, categorie, idVendeur, rue, codePostal, ville);
+				articleManager.modifierArticle(idArticle, nom, description, debutEnchere, finEnchere, prix, categorie, rue, codePostal, ville);
 			} catch (BusinessException e) {
-				e.printStackTrace();
 				request.setAttribute("listeCodesErreur",e.getListeCodesErreur());
+				try {
+					request.setAttribute("objArticle", articleManager.afficherArticle(idArticle));
+				} catch (BusinessException e1) {
+					request.setAttribute("listeCodesErreur",e.getListeCodesErreur());
+				}
+				request.getRequestDispatcher("WEB-INF/nouvelle_vente.jsp").forward(request, response);
 			}
 		}
-	}
-	
-	
-	private void modifierVente(HttpServletRequest request, HttpServletResponse response) {
-		
 	}
 	
 	
 	
 	private int lireParametreIdArticle(HttpServletRequest request, List<Integer> listeCodesErreur) {
 		int id = 0;
-		try
-		{
-			if(request.getParameter("idArticle")!=null)
-			{
+		try {
+			if (request.getParameter("idArticle") != null) {
 				id = Integer.parseInt(request.getParameter("idArticle"));
 			}
-		}
-		catch(NumberFormatException e)
-		{
-			e.printStackTrace();
-//			listeCodesErreur.add(CodesResultat Servlets.FORMAT_ID_ARTICLE_ERREUR);
+		} catch(NumberFormatException e) {
+			listeCodesErreur.add(CodesResultatServlets.FORMAT_NO_ARTICLE_ERREUR);
 		}
 		return id;
 	}
@@ -143,7 +200,7 @@ public class ServletNouvelleVente extends HttpServlet {
 		String nom;
 		nom = request.getParameter("nom");
 		if (nom == null || nom.trim().equals("")) {
-//			listeCodesErreur.add(CodesResultatServlets.NOM_LISTE_OBLIGATOIRE);
+			listeCodesErreur.add(CodesResultatServlets.NOM_D_ARTICLE_OBLIGATOIRE);
 		}
 		return nom;
 	}
@@ -152,7 +209,7 @@ public class ServletNouvelleVente extends HttpServlet {
 		String description;
 		description = request.getParameter("description");
 		if (description == null || description.trim().equals("")) {
-//			listeCodesErreur.add(CodesResultatServlets.NOM_LISTE_OBLIGATOIRE);
+			listeCodesErreur.add(CodesResultatServlets.DESCRIPTION_ARTICLE_OBLIGATOIRE);
 		}
 		return description;
 	}
@@ -161,7 +218,7 @@ public class ServletNouvelleVente extends HttpServlet {
 		LocalDateTime debutEnchere;
 		debutEnchere = LocalDateTime.parse(request.getParameter("debutEnchere"));
 		if (debutEnchere == null) {
-//			listeCodesErreur.add(CodesResultatServlets.NOM_LISTE_OBLIGATOIRE);
+			listeCodesErreur.add(CodesResultatServlets.DATE_DEBUT_ENCHERE_OBLIGATOIRE);
 		}
 		return debutEnchere;
 	}
@@ -170,7 +227,7 @@ public class ServletNouvelleVente extends HttpServlet {
 		LocalDateTime finEnchere;
 		finEnchere = LocalDateTime.parse(request.getParameter("finEnchere"));
 		if (finEnchere == null) {
-//			listeCodesErreur.add(CodesResultatServlets.NOM_LISTE_OBLIGATOIRE);
+			listeCodesErreur.add(CodesResultatServlets.DATE_FIN_ENCHERE_OBLIGATOIRE);
 		}
 		return finEnchere;
 	}
@@ -179,12 +236,11 @@ public class ServletNouvelleVente extends HttpServlet {
 		int id = 0;
 		
 		try {
-			if (request.getParameter("id") != null) {
-				id = Integer.parseInt(request.getParameter("id"));
+			if (request.getParameter("idVendeur") != null) {
+				id = Integer.parseInt(request.getParameter("idVendeur"));
 			}
 		} catch (NumberFormatException e) {
-			e.printStackTrace();
-//			listeCodesErreur.add(CodesResultatServlets.FORMAT_ID_ARTICLE_ERREUR);
+			listeCodesErreur.add(CodesResultatServlets.FORMAT_ID_USER);
 		}
 		
 		return id;
@@ -194,7 +250,7 @@ public class ServletNouvelleVente extends HttpServlet {
 		String rue;
 		rue = request.getParameter("rue");
 		if (rue == null || rue.trim().equals("")) {
-//			listeCodesErreur.add(CodesResultatServlets.NOM_LISTE_OBLIGATOIRE);
+			listeCodesErreur.add(CodesResultatServlets.RUE_OBLIGATOIRE);
 		}
 		return rue;
 	}
@@ -203,7 +259,7 @@ public class ServletNouvelleVente extends HttpServlet {
 		String codePostal;
 		codePostal = request.getParameter("codePostal");
 		if (codePostal == null || codePostal.trim().equals("")) {
-//			listeCodesErreur.add(CodesResultatServlets.NOM_LISTE_OBLIGATOIRE);
+			listeCodesErreur.add(CodesResultatServlets.CODE_POSTAL_OBLIGATOIRE);
 		}
 		return codePostal;
 	}
@@ -212,7 +268,7 @@ public class ServletNouvelleVente extends HttpServlet {
 		String ville;
 		ville = request.getParameter("ville");
 		if (ville == null || ville.trim().equals("")) {
-//			listeCodesErreur.add(CodesResultatServlets.NOM_LISTE_OBLIGATOIRE);
+			listeCodesErreur.add(CodesResultatServlets.VILLE_OBLIGATOIRE);
 		}
 		return ville;
 	}
